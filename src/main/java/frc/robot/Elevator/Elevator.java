afea96a716d63090ba3aea50dd0c13012439f2a2
Gsahaj <sahaj06chawla@gmail.com>
2025-03-02 21:38:23 -0500

    Added JoyStick Code

---
+++ src/main/java/frc/robot/Elevator/Elevator.java
@@ -0,0 +1,36 @@
+package frc.robot.Elevator;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Elevator extends SubsystemBase{
+    //Hardware
+    private final PWMSparkMax motor;
+    private final DigitalInput toplimitswitch;
+    private final DigitalInput bottomlimitswitch;
+
+
+    public Elevator(){
+        motor = new PWMSparkMax(0); //subject to change
+        motor.setSafetyEnabled(true);
+
+        toplimitswitch = new DigitalInput(1); //subject to change 
+        bottomlimitswitch = new DigitalInput(2);//subject to change
+
+    }
+
+    public void setSpeed(double speed){
+        //Check is limit switches are tiggered and stop motor accordingly
+        if ((speed > 0 && toplimitswitch.get()) || (speed < 0 && bottomlimitswitch.get())){
+            motor.set(0); //Stop movement if limit switch is trigger
+        }
+        else{
+            motor.set(speed);
+        }
+    }
+
+    public void stop(){
+        motor.set(0); //Ensure motor is stopped
+    }
+}
