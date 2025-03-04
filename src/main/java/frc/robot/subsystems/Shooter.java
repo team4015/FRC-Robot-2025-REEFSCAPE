b076132fd4b25bacc7be9273900dd3ff85ccdc6a
teugegd <150204718+teugegd@users.noreply.github.com>
2025-03-04 14:54:16 -0500

    Update Shooter.java

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -7,11 +7,9 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase
 {
-
     private final PWMSparkMax motor;
     private final XboxController controller;
     private final DigitalInput limitswitch;
-
 
     public Shooter()
     {
@@ -22,16 +20,18 @@ public class Shooter extends SubsystemBase
         limitswitch = new DigitalInput(0); //subject to change
     }
 
-
     public void shoot(double speed)
     {
         if (controller.getRightBumperButtonPressed()) motor.set(speed);   
     }
 
-
     public void preventEject()
     {
         if (limitswitch.get() && !controller.getRightBumperButtonPressed()) motor.set(0);
     }
-        
+
+    public void stop()
+    {
+        motor.set(0);
+    }  
 }
