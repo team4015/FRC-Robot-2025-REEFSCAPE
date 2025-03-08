96a7aab33ce4395259093f75000d44de03bde8a6
Gsahaj <sahaj06chawla@gmail.com>
2025-03-08 09:27:48 -0500

    Update BindingControls.java

--- src/main/java/frc/robot/controls/BindingControls.java
+++ src/main/java/frc/robot/controls/BindingControls.java
@@ -1,12 +1,17 @@
 package frc.robot.controls;
 
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.Auto.AutonomousCommand;
+import frc.robot.commands.Elevator.JoystickElevatorControls;
+import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Elevator;
 
 //This class will assign the joystick controll commands as default for the elevator 
 public class BindingControls {
     private final Elevator elevator = new Elevator();
     private final XboxController controller = new XboxController(0); //Subject to change
+    private final AutonomousCommand autonomouscommand = new AutonomousCommand();
 
     public BindingControls(){
         configureBindings(); //Call Method
@@ -15,5 +20,8 @@ public class BindingControls {
     private void configureBindings(){
         //Set joystick control as the deafult command for the elevator
         elevator.setDefaultCommand(new JoystickElevatorControls(elevator, controller));
+    }
+    public Command getAutonomousCommand(){
+        return autonomouscommand;
     }
 }
