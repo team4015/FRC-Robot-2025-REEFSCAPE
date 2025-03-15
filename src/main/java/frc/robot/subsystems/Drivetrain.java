707c6920f14adb7df866e2c080f3169f2c835064
Gsahaj <sahaj06chawla@gmail.com>
2025-03-15 14:00:06 -0400

    Getting new code

---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,238 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import edu.wpi.first.wpilibj.motorcontrol.Victor;
+//import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+
+import edu.wpi.first.wpilibj.BuiltInAccelerometer;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.Timer;
+
+import edu.wpi.first.wpilibj.XboxController; // new
+
+//import edu.wpi.first.apriltag.jni.*;
+
+/*import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;*/
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Drivetrain extends SubsystemBase
+{
+  // CONSTANTS //
+
+  
+  // declare constant variables here (make them private static)
+  
+  // VARIABLES //
+  
+  
+  //controller
+  private final XboxController controller;
+  
+  // HARDWARE //
+  private final MotorController rightMotor;
+  private final MotorController leftMotor; 
+  /*private final MotorController rightFrontMotor;
+  private final MotorController rightBackMotor; 
+  private final MotorController leftFrontMotor;
+  private final MotorController leftBackMotor;*/
+  private DifferentialDrive drive;
+
+  private ADXRS450_Gyro gyro;
+  private BuiltInAccelerometer accel;
+  private Timer timer; 
+  private Timer driveStraightTimer; 
+
+   // declare variables here
+   private double currentAngle;
+   private double currentGyroAngle;
+   private double offSetAngle;
+   private boolean tilting;
+   private double timeElapsed; 
+
+   //private final DifferentialDriveOdometry odometry; 
+  // declare electrical hardware here (PORTS)
+  public static final int RIGHT_MOTOR =0 ; 
+  public static final int LEFT_MOTOR = 1;
+  
+  //ports for encoders
+  private final static double ACCEL_TO_CENTRE_DIST = 0.305;//in meters 
+  private final static double ACCEL_ANGLE = 66.9; //in degrees
+  private final static double COS_OF_ACCEL_ANGLE = Math.cos(Math.toRadians(ACCEL_ANGLE));  
+
+  // CONSTANTS // 
+  public static final double WHEEL_RADIUS =  Units.inchesToMeters(6)/2;
+  public static double distanceFromStartingPos = 0; 
+  public final static double GEAR_RATIO = 1/10.71;
+  public final static double driveStraightCorrectTime = 0.001; 
+
+  
+  
+  // CONSTRUCTORS //
+  
+  public Drivetrain()
+  {
+    // instantiate all electrical hardware and variable here
+    rightMotor = new Victor(LEFT_MOTOR);
+    leftMotor = new Talon(RIGHT_MOTOR);
+    leftMotor.setInverted(true);
+
+    distanceFromStartingPos = 0;
+    currentAngle = 0;
+    currentGyroAngle = 0;
+    offSetAngle = 0;
+    tilting = false; 
+
+    drive = new DifferentialDrive(leftMotor, rightMotor);
+    
+    gyro = new ADXRS450_Gyro();
+    calibrateGyro();
+
+    accel = new BuiltInAccelerometer();
+    
+    timer = new Timer();
+    driveStraightTimer = new Timer(); 
+    resetTimer(); 
+    startTimer();
+  }
+  
+  public void moveMotors(double speed, double turn){
+    SmartDashboard.putNumber("Gyro Rate:", gyro.getRate());
+    drive.arcadeDrive(speed, turn);
+    /*if(turn == 0){
+      if(gyro.getAngle()>currentAngle){
+        offSetAngle = gyro.getAngle()-currentAngle;
+      }else if(gyro.getAngle()<currentAngle){
+        offSetAngle = currentAngle-gyro.getAngle();
+      }
+      if(offSetAngle==0||Math.abs(offSetAngle)%360.0==0){
+        drive.arcadeDrive(speed,turn);
+      }else{
+        driveStraightTimer.start();
+        if(driveStraightTimer.get()<=driveStraightCorrectTime&&driveStraightTimer.get()>0){
+          drive.arcadeDrive(0,(-Math.toRadians(offSetAngle)/driveStraightCorrectTime)*WHEEL_RADIUS);
+        }
+        driveStraightTimer.reset();
+      }
+    }else{
+      drive.arcadeDrive(speed, turn);
+    }*/
+  }
+
+  public void stopMotors(){
+    drive.stopMotor();
+  }
+
+  public void calibrateGyro(){
+    if (controller.getBackButtonPressed())
+    {
+      resetGyro();
+      gyro.calibrate();
+    }
+  }
+
+  public void resetGyro(){
+    gyro.reset();
+  }
+
+  public double getGyroAngle(){
+    return gyro.getAngle(); 
+  }
+
+  /*public Rotation2d gyroRotation2d(){
+    return gyro.getRotation2d();
+  }*/
+
+  public void resetGyroAngle(){
+    currentAngle = 0;
+  }
+
+  public double reduceAngle(Double gyroAngle){
+    int degreeCount = 0;
+    for(int i = 1; i<=Math.abs(gyroAngle);i++){
+      if(Math.abs(gyroAngle)-(180*i)>=0){
+        degreeCount++; 
+      }
+    }
+    return Math.abs(gyroAngle)-(degreeCount*Math.abs(gyroAngle));    
+  }
+
+  public double getTime(){
+    return timer.get();
+  }
+
+  public void startTimer(){
+    timer.start();
+  }
+
+  public void stopTimer(){
+    timer.stop();
+  }
+
+  public void resetTimer(){
+    timer.reset(); 
+  }
+
+  public boolean checkForTilt(){
+    if(accel.getZ()!=0){
+      tilting = true;     
+    }else{
+      tilting = false;
+    }
+    SmartDashboard.putBoolean("Tilting:", tilting);
+    return tilting; 
+  }
+
+  @Override 
+  public void periodic (){
+
+    timeElapsed =  getTime();
+    currentGyroAngle = getGyroAngle();
+    double xAccel = -accel.getX();
+    xAccel *= 9.8;
+
+    double rate = gyro.getRate();
+    rate = Math.toRadians(rate);
+
+    double turningAcceleration = rate*rate*ACCEL_TO_CENTRE_DIST*COS_OF_ACCEL_ANGLE;// Thank you Lucas's physics class XDXD --> btw
+                                                                              //btw you get this by taking the second derivative of rcos(angular speed*time = rad) which equals angular displacement
+    xAccel+= turningAcceleration;
+    
+    
+    SmartDashboard.putNumber("Gyro Angle:", currentGyroAngle);
+    SmartDashboard.putNumber("Acceleration:", xAccel);
+    
+
+    double distanceInX = xAccel*Math.pow(timeElapsed,2);
+    if(distanceFromStartingPos==0){
+      if(currentGyroAngle==0){
+        distanceFromStartingPos+=distanceInX;
+      }else{
+        distanceFromStartingPos+=distanceInX/Math.cos(currentGyroAngle); 
+      }
+    }else{
+      if(reduceAngle(currentGyroAngle)>90){
+        distanceFromStartingPos = Math.pow((Math.pow(distanceFromStartingPos,2)+Math.pow(distanceInX,2)-2*distanceInX*distanceFromStartingPos*Math.cos((90-reduceAngle(currentGyroAngle)))),0.5);
+      }else if(reduceAngle(currentGyroAngle)<90){
+        distanceFromStartingPos = Math.pow((Math.pow(distanceFromStartingPos,2)+Math.pow(distanceInX,2)-2*distanceInX*distanceFromStartingPos*Math.cos((90+reduceAngle(currentGyroAngle)))),0.5);
+      }else{
+        distanceFromStartingPos = Math.pow(((Math.pow(distanceFromStartingPos,2))+Math.pow(distanceInX,2)),0.5);
+      }
+    }
+
+    SmartDashboard.putNumber("Distance from Start:", distanceFromStartingPos);
+    resetGyroAngle(); 
+    resetTimer();
+  }
+
+  // METHODS //
+  
+  // write your subsystem methods here
+}
---
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -0,0 +1,64 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Elevator extends SubsystemBase{
+    //Hardware
+    private final PWMSparkMax motor;
+    private final XboxController controller;
+    private final DigitalInput levellimitswitch;
+
+    //variables declared for variable counting
+    int levelCounter;
+    int targetCounter;
+
+    public Elevator(){
+        motor = new PWMSparkMax(4); //subject to change
+        motor.setSafetyEnabled(true);
+
+        levellimitswitch = new DigitalInput(0);
+
+        controller = new XboxController(0); //Subject to change
+    }
+
+    public void setSpeed(double speed){
+        motor.set(speed);
+        //Check joystick and see which button is pressed and moves according to different levels using limit switch
+        if(controller.getXButtonPressed()){
+            levelCounter = 1;
+        }
+        if(controller.getYButtonPressed()){
+            levelCounter = 2;
+        }
+        if(controller.getBButton()){
+            levelCounter = 3;
+        }
+        if(controller.getAButton()){
+            levelCounter = 4;
+        }
+
+        targetCounter = 0;
+
+        while(levelCounter == targetCounter){
+            motor.set(speed);
+            if(levellimitswitch.get() == true){
+                //increment target counter by one when limit switch is pressed
+                targetCounter += 1;
+            }
+            if(levelCounter == targetCounter){
+                //set motors to 0 ---> stop motors
+                //reset the targetCounter and levelCounter to 0 respectively
+                motor.set(0);
+                targetCounter = 0;
+                levelCounter = 0;
+            }
+        }
+    }
+
+    public void stop(){
+        motor.set(0); //Ensure motor is stopped
+    }
+}
